eedfa2410a45eaaf81a8577c531f6e9a6044fe9e
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-03-23 11:29:07 +0100

    add food_fact_api and data

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -12,6 +12,7 @@ class FoodsController < ApplicationController
     @food = Food.new(pooh_params)
     @meal = Meal.find(params[:meal_id])
     @day = Day.find(params[:day_id])
+    open_food_fact(params[:food][:name])
     if @food.save
       MealFood.create!(food_id: @food.id, meal_id: @meal.id)
       redirect_to day_meal_path(@day.id, @meal.id)
@@ -27,5 +28,12 @@ class FoodsController < ApplicationController
 
   def pooh_params
     params.require(:food).permit(:name)
+  end
+
+  def open_food_fact(name)
+    require 'openfoodfacts'
+    products = Openfoodfacts::Product.search(name, locale: 'fr', page_size: 1)
+    ingredients = products.first.fetch.ingredients
+    raise
   end
 end
