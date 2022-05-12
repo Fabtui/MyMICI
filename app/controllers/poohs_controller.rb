e892d1ee2a15bafabe0bf7fc0d0e1d5934f0cc93
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-05-12 19:48:01 +0200

    add edit action on pooh

--- app/controllers/poohs_controller.rb
+++ app/controllers/poohs_controller.rb
@@ -1,4 +1,8 @@
 class PoohsController < ApplicationController
+  def show
+    @pooh = Pooh.find(params[:id])
+  end
+
   def new
     @day = Day.find(params[:day_id])
     @pooh = Pooh.new
@@ -14,7 +18,16 @@ class PoohsController < ApplicationController
     end
   end
 
+  def edit
+    @pooh = Pooh.find(params[:id])
+    @day = Day.find(@pooh.day.id)
+  end
+
   def update
+    @pooh = Pooh.find(params[:id])
+    @pooh.update(pooh_params)
+    @day = Day.find(@pooh.day.id)
+    redirect_to day_path(@day.id)
   end
 
   def delete
